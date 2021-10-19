bf38a70cbee028813b94e27615f37fb699f0f6ca
Marco Bolis <mbolis1984@gmail.com>
2021-10-19 16:35:08 +0200

    Add solutions - At my signal, unleash hell

--- public/sprites/js/app.js
+++ public/sprites/js/app.js
@@ -1,11 +1,87 @@
 "use strict";
 
-// All seems to work well, but... What if the connection is not good?
+/**
+ * @template T
+ * @callback OnLoaded
+ * @param {Error} error
+ * @param {T} [payload]
+ */
 
-// TODO: preload images
-// How to orchestrate?
-// - one after the other?
-// - in parallel?
+/**
+ * Load one image, call `onLoad` when it is loaded.
+ * @param {string} url
+ * @param {OnLoaded<HTMLImageElement>} onLoad
+ */
+function preloadImage(url, onLoad) {
+  const image = new Image();
+  image.onload = () => onLoad(null, image);
+  image.onerror = () => onLoad(new Error("Could not load image: " + url));
+  image.src = url;
+
+  return image;
+}
+
+/**
+ * Load all images in sequence, call `onLoad` when they are **all** loaded.
+ * @param {string[]} urls
+ * @param {OnLoaded<HTMLImageElement[]>} onLoad
+ */
+function preloadAllImagesSequence(urls, onLoad) {
+  const images = [];
+  preloadImage(urls.shift(), function doWork(err, image) {
+    if (err) return onLoad(err);
+
+    images.push(image);
+    if (!urls.length) onLoad(null, images);
+    else preloadImage(urls.shift(), doWork);
+  });
+
+  return images; // Will be empty at first...
+}
+
+/**
+ * Load all images, call `onLoad` when they are **all** loaded.
+ * @param {string[]} urls
+ * @param {OnLoaded<HTMLImageElement[]>} onLoad
+ */
+function preloadAllImages(urls, onLoad) {
+  let remaining = 0;
+  let crashed = false;
+
+  const images = urls.map((url) => {
+    remaining++;
+    return preloadImage(url, (err) => {
+      if (crashed) return;
+      if (err) {
+        crashed = true;
+        onLoad(err);
+        return;
+      }
+      if (!--remaining) onLoad(null, images);
+    });
+  });
+
+  return images;
+}
+
+/**
+ * Load all images, call `onLoad` when **any** is loaded or fails.
+ * @param {string[]} urls
+ * @param {OnLoaded<HTMLImageElement>} onLoad
+ */
+function preloadAnyImage(urls, onLoad) {
+  let done = false;
+
+  return urls.map((url) =>
+    preloadImage(url, (err, image) => {
+      if (done) return;
+      if (err) return onLoad(err);
+
+      done = true;
+      onLoad(null, image);
+    })
+  );
+}
 
 const NAMES = [
   "Garrett",
@@ -18,6 +94,10 @@ const NAMES = [
   "Remedia",
 ];
 
+function spriteUrl(sprite) {
+  return `images/${sprite.value}.png`;
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   const spriteSelect = document.getElementById("sprite");
   const spriteImg = document.getElementById("sheet_display");
@@ -27,22 +107,38 @@ document.addEventListener("DOMContentLoaded", () => {
   const characterTemplate = document.querySelector(".character");
   const templateFactory = makeTemplateFactory(characterTemplate);
 
-  let template;
-
-  spriteImg.onload = () => {
-    document.body.classList.remove("loading");
-
-    // cleanup
-    template && template.destroy();
-    showcase.textContent = "";
-
-    // draw
-    template = templateFactory(spriteImg);
-    showcase.append(...NAMES.map(template));
-  };
+  const spriteOptions = [...spriteSelect.querySelectorAll("option")];
+  const spriteUrls = spriteOptions.map(spriteUrl);
 
   spriteSelect.onchange = () => {
     document.body.classList.add("loading");
-    spriteImg.src = `images/${spriteSelect.value}.png`;
+    spriteImg.src = spriteUrl(spriteSelect);
   };
+
+  preloadAnyImage(spriteUrls, (err, image) => {
+    if (err) return alert(`Error! ${err.message}`);
+
+    let template;
+
+    spriteImg.onload = displayShowcase;
+    if (image instanceof HTMLImageElement) {
+      const [, value] = /\/([^\/]+)\.png$/.exec(image.src);
+      spriteSelect.value = value;
+      spriteSelect.onchange();
+    } else {
+      displayShowcase();
+    }
+
+    function displayShowcase() {
+      document.body.classList.remove("loading");
+
+      // cleanup
+      template && template.destroy();
+      showcase.textContent = "";
+
+      // draw
+      template = templateFactory(spriteImg);
+      showcase.append(...NAMES.map(template));
+    }
+  });
 });
