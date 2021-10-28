9bb2736a164de1e07bed7e051cf2bb42bcc84a1e
Marco Bolis <mbolis1984@gmail.com>
2021-10-28 12:55:50 +0200

    Abstraction

--- public/app.js
+++ public/app.js
@@ -1,20 +1,22 @@
 "use strict";
 
-const itemsEl = findPlaylistItemsContainerEl();
-let childEl;
-while ((childEl = itemsEl.firstChild)) {
-  const wasRemoved = removeIfNotSelected(childEl);
-  if (!wasRemoved) break;
+function saveData(data, callback) {
+  console.log(data);
+  callback("done");
 }
 
-/**
- * @returns {HTMLUListElement}
- */
-function findPlaylistItemsContainerEl() {}
-
-function removeIfNotSelected(el) {
-  if (!el.classList.contains("selected")) {
-    el.remove();
-  }
-  return !el.classList.contains("not-selected"); // this is really bad: mysterious, surprising, hidden dependency
+async function fetchSaveData(data, callback) {
+  await fetch("/api/projects", {
+    method: "POST",
+    body: JSON.stringify(data),
+  });
+  callback("done");
 }
+
+function doSomething(saveData) {
+  const data = { a: 1, b: 2 };
+  saveData(data, console.log); // interface := (data, callback) => {}
+}
+
+doSomething(saveData); // <- inject saveData
+doSomething(fetchSaveData); // <- inject fetchSaveData
