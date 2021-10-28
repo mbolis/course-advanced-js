beb9cdbd490fe8c094e61e4783754d97edf9f78b
Marco Bolis <mbolis1984@gmail.com>
2021-10-28 12:23:32 +0200

    Maybe a solution

--- public/app.js
+++ public/app.js
@@ -1,12 +1,20 @@
 "use strict";
 
-var x = theElement(); // ???
-var c;
-while ((c = x.firstChild)) {
-  c = procDElem(c); // ???
-  if (c) break;
+const itemsEl = findPlaylistItemsContainerEl();
+let childEl;
+while ((childEl = itemsEl.firstChild)) {
+  const wasRemoved = removeIfNotSelected(childEl);
+  if (!wasRemoved) break;
 }
 
-function theElement() {}
+/**
+ * @returns {HTMLUListElement}
+ */
+function findPlaylistItemsContainerEl() {}
 
-function procDElem() {}
+function removeIfNotSelected(el) {
+  if (!el.classList.contains("selected")) {
+    el.remove();
+  }
+  return !el.classList.contains("not-selected"); // this is really bad: mysterious, surprising, hidden dependency
+}
