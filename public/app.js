a8256aaaf9bee3d783e7cdaa54e8391f526df9e4
Marco Bolis <mbolis1984@gmail.com>
2021-10-28 11:15:51 +0200

    Bad Code

--- public/app.js
+++ public/app.js
@@ -1,1 +1,12 @@
 "use strict";
+
+var x = theElement(); // ???
+var c;
+while ((c = x.firstChild)) {
+  c = procDElem(c); // ???
+  if (c) break;
+}
+
+function theElement() {}
+
+function procDElem() {}
